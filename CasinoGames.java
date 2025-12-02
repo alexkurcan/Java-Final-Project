00a733963b93925f73be2915e0279f7d8aa447cd
alexkurcan <alxkurcan56@outlook.com>
2025-12-02 17:40:59 -0500

    making the abtract file

---
+++ CasinoGames.java
@@ -0,0 +1,45 @@
+/* 
+Abstract parent class for various casino games
+*/
+
+public abstract class CasinoGames {
+
+    // Parent data members
+    protected String gameName;
+    protected int numPlayers;
+    protected int numCards;
+    protected double costToPlay;
+    protected int numWins;
+    protected int numLosses;
+
+    // Constructor
+    public CasinoGames(String gameName, int numPlayers, int numCards, double costToPlay) {
+        this.gameName = gameName;
+        this.numPlayers = numPlayers;
+        this.numCards = numCards;
+        this.costToPlay = costToPlay;
+        this.numWins = 0;
+        this.numLosses = 0;
+    }
+
+    // Accessors / Mutators
+    public String getGameName() { return gameName; }
+    public int getNumPlayers() { return numPlayers; }
+    public int getNumCards() { return numCards; }
+    public double getCostToPlay() { return costToPlay; }
+    public int getNumWins() { return numWins; }
+    public int getNumLosses() { return numLosses; }
+
+    public void setNumPlayers(int n) { numPlayers = n; }
+
+    public void addWin() { numWins++; }
+    public void addLoss() { numLosses++; }
+
+    // Common parent method (can be overridden)
+    public double calculateProfit() {
+        return (numWins * costToPlay) - (numLosses * costToPlay);
+    }
+
+    // Abstract method → all child classes must implement gameplay
+    public abstract void playRound();
+}
