d548ac1899d8c7eb0d496956d2811cde0d1b7b6f
ToriTalks2much <x68gh5zdjt@privaterelay.appleid.com>
2025-12-05 09:52:01 -0500

    Enhance slot.java with comments and documentation
    Added comments and documentation for clarity.

--- slot.java
+++ slot.java
@@ -1,4 +1,12 @@
+/* 
+Afton Pate 
+12/5/25
+This program simulates a  slot machine
+*/
+
+
 import java.util.*;
+// My slot class, includes wins, losses, and bet and stuff 
 public class slot {
     private int num1;
     private int num2;
@@ -16,12 +24,13 @@ public class slot {
     public slot(int bet) {
         this.bet = bet;
     }
-    
+    // This is MY clear function. IT makes the code look better while running
     public static void clear() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }    
 
+    // This part finds 3 random numbers and prints them out 
     public void play() {
         num1 = (int)(Math.random() * 10) + 1;
         num2 = (int)(Math.random() * 10) + 1;
@@ -30,6 +39,7 @@ public class slot {
         System.out.println(num1 + " " + num2 + " " + num3);
     }
 
+    // This code finds how many numbers are the same and gives back the bet accordingly 
     public int outcome() {
         if (num1 == num2 && num2 == num3) {
             balance += bet * 5;
@@ -49,7 +59,7 @@ public class slot {
     public static int getCount() {
         return count;
     }
-
+    // Runner / tester / Main 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         System.out.println("Enter the starting balance: ");
